f77c90cac94cf236e6147247caa085aa382b68a3
Mark Yarchak <markyarchak.work@gmail.com>
2020-03-25 16:28:12 +0200

    sync init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+// import { Plugins, PushNotification, PushNotificationToken, PushNotificationActionPerformed } from '@capacitor/core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+// const { PushNotifications } = Plugins;
 
 if (environment.production) {
   enableProdMode();
 }
 
+// PushNotifications.register();
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+defineCustomElements(window);
